54406baf44659653fa38d9855cf3f70626baf283
Popova Marina <104079683+compotee@users.noreply.github.com>
2022-09-20 19:34:57 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@ function generateRandom(from, before) {
   if (from < 0) {
     from = 0;
   }
+  
   if (before < 0) {
     before = 0;
   }
-  if (from === before){
+  
+  if (from === before) {
     return from;
   }
-  if (from > before){
+  
+  if (from > before) {
     return Math.round(before + Math.random() * (from - before));
   }
   return Math.round(from + Math.random() * (before - from));
@@ -17,7 +20,7 @@ function generateRandom(from, before) {
 function checkLength(verifiedString, maxLength) {
   if (verifiedString.length >= maxLength) {
     return false;
-  }else{
+  } else {
     return true;
   }
 }
