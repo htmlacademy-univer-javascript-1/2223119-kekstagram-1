798d21ebfb382090584173ff1ccccb8dbc709f3f
Popova Marina <104079683+compotee@users.noreply.github.com>
2022-10-11 18:28:04 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,13 +1,13 @@
 import {generateRandom, DESCRIPTIONS, COMMENTS, NAMES} from './data.js';
 
-function generateComment(i) {
+function generateComment(number) {
   const message = Array(generateRandom(1, 2));
   for (let i = 0; i < message - 1; i++) {
     message[i] = COMMENTS[generateRandom(0, COMMENTS.length - 1)];
   }
 
   return {
-    id: i,
+    id: number,
     avatar: `img/avatar-${generateRandom(1, 6)}.svg`,
     message: message,
     name: NAMES[generateRandom(0, NAMES.length - 1)]
